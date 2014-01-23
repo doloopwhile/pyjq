83b0fe7895091e9fe3eb1352dcd7cab0f6ec8a7d
OMOTO Kenji <kenji.omoto@hde.co.jp>
2014-01-23 12:16:07 +0900

    Added  argument, and removed kw argument

--- pyjq.py
+++ pyjq.py
@@ -2,32 +2,33 @@ import _pyjq
 __all__ = []
 
 
-def compile(script, **kw):
+def compile(script, vars={}):
     """
     Compile a jq script, retuning a script object.
     """
-    return _pyjq.Script(script.encode('utf-8'), kw)
+    return _pyjq.Script(script.encode('utf-8'), vars={})
 
 
-def all(script, value, **kw):
+def all(script, value, vars={}):
     """
     Transform object by jq script, returning all results as list.
     """
-    return compile(script, **kw).apply(value)
+    return compile(script, vars).apply(value)
 
 apply = all
 
-def first(script, value, default=None, **kw):
+
+def first(script, value, default=None, vars={}):
     """
     Transform object by jq script, returning the first result.
     Return default if result is empty.
     """
-    return compile(script, **kw).first(value, default)
+    return compile(script, vars).first(value, default)
 
 
-def one(script, value, **kw):
+def one(script, value, vars={}):
     """
     Transform object by jq script, returning the first result.
     Raise ValueError unless results does not include exactly one element.
     """
-    return compile(script, **kw).one(value)
+    return compile(script, vars).one(value)
