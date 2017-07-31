f86ece3de48d3260b04261eee9c66a64604bed1c
Mark Feit <mfeit@internet2.edu>
2017-07-31 17:46:41 -0400

    Add library paths to all functions.

--- pyjq.py
+++ pyjq.py
@@ -47,33 +47,33 @@ def _get_value(value, url, opener):
     return value
 
 
-def all(script, value=None, vars={}, url=None, opener=default_opener):
+def all(script, value=None, vars={}, url=None, opener=default_opener, library_paths=None):
     """
     Transform value by script, returning all results as list.
     """
-    return compile(script, vars).all(_get_value(value, url, opener))
+    return compile(script, vars, library_paths).all(_get_value(value, url, opener))
 
 
-def apply(script, value=None, vars={}, url=None, opener=default_opener):
+def apply(script, value=None, vars={}, url=None, opener=default_opener, library_paths=None):
     """
     Transform value by script, returning all results as list.
     """
-    return all(script, value, vars, url, opener)
+    return all(script, value, vars, url, opener, library_paths)
 
 apply.__doc__ = all.__doc__
 
 
-def first(script, value=None, default=None, vars={}, url=None, opener=default_opener):
+def first(script, value=None, default=None, vars={}, url=None, opener=default_opener, library_paths=None):
     """
     Transform object by jq script, returning the first result.
     Return default if result is empty.
     """
-    return compile(script, vars).first(_get_value(value, url, opener), default)
+    return compile(script, vars, library_paths).first(_get_value(value, url, opener), default)
 
 
-def one(script, value=None, vars={}, url=None, opener=default_opener):
+def one(script, value=None, vars={}, url=None, opener=default_opener, library_paths=None):
     """
     Transform object by jq script, returning the first result.
     Raise ValueError unless results does not include exactly one element.
     """
-    return compile(script, vars).one(_get_value(value, url, opener))
+    return compile(script, vars, library_paths).one(_get_value(value, url, opener))
